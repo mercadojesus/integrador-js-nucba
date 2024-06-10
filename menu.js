b493e2ca02df1f96562bdb10f81bf9ac22a5dd04
mercadojesus <mercado_jesus@outlook.es>
2024-06-09 21:36:32 -0300

    9° Actualizacion integrador JS - Nucba

---
+++ menu.js
@@ -0,0 +1,17 @@
+let enlaces = document.querySelectorAll("#enlace")
+const openMenu = document.querySelector("#nav-responsive");
+const closeMenu = document.querySelector("#close-responsive");
+const menu = document.querySelector("#nav");
+
+openMenu.addEventListener("click", () => {
+    menu.classList.add("menu-responsive");
+})
+
+closeMenu.addEventListener("click", () => {
+    menu.classList.remove("menu-responsive");
+})
+
+enlaces.addEventListener("click", () => {
+    console.log("clickeaste en un enlace man!!!");
+    // menu.classList.remove("menu-responsive");
+})
